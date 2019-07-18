a810fafa2b736ceb0bb95b14eab734f77bfafb9c
villa1013 <williamvillalba13@gmail.com>
2019-07-18 14:45:30 -0500

    Front landing

---
+++ README.md
@@ -0,0 +1,12 @@
+# Landing TM
+
+## TecHnology
+
+- Html5
+- CSS3
+- Javascript
+- json
+- stylus
+- gulp
+- git
+- npm
---
+++ assets/js/app.js
@@ -0,0 +1,88 @@
+// Autor: William Villalba
+// Email: williamvillalba13@gmail.com
+
+
+// =========== FUNCTIONS GENERALS ===================
+//PRELOAD
+function load() {
+  var preloader = document.getElementById("preload");
+  setTimeout(function() {
+    preloader.classList.add("close");
+  }, 1000);
+}
+window.onload = load;
+
+
+//FIXED NAV
+window.onscroll = function () {
+  var buttonid = document.getElementById("header");
+  if(document.documentElement.scrollTop >= 50){
+    buttonid.classList.add("fixed");
+  }else{
+    buttonid.classList.remove("fixed");
+  }
+};
+
+
+// OPEN NAV
+function openNav() {
+  var element = document.getElementById("page");
+  if (element.classList) { 
+    element.classList.toggle("open");
+  } else {
+    var classes = element.className.split(" ");
+    var i = classes.indexOf("open");
+
+    if (i >= 0) 
+      classes.splice(i, 1);
+    else 
+      classes.push("open");
+      element.className = classes.join(" "); 
+  }
+}
+
+
+//JSON IMAGES
+var xmlhttp = new XMLHttpRequest();
+var url = "assets/js/dataslider.json";
+
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var myArr = JSON.parse(this.responseText);
+    loadDataJson(myArr);
+  }
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+function loadDataJson(arr) {
+  var out = "";
+  var i;
+  for(i = 0; i < arr.length; i++) {
+    out += '<li class="item-slides"><h1>' + arr[i].text + '</h1><img src="' + 
+    arr[i].url + '"></li>';
+  }
+  document.getElementById("dataImages").innerHTML = out;
+}
+
+
+
+//SLIDER INIT
+var slideIndex = 1;
+showDivs(slideIndex);
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("item-slides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+     x[i].classList.remove("active");     
+  }
+  x[slideIndex-1].style.display = "block"; 
+  x[slideIndex-1].classList.add("active");
+}
+// document.querySelectorAll('.item-slides')[0].style.display = "block";
